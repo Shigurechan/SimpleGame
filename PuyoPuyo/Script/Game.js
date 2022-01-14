62e4c578f8d124d1d043b263e76c599013cd11a7
DESKTOP-EAO62H6\Matsuzawa ken <0yw3250118.2g2g@gmail.com>
2022-01-14 19:07:16 +0900

    ぷよ削除を実装

--- PuyoPuyo/Script/Game.js
+++ PuyoPuyo/Script/Game.js
@@ -1,33 +1,50 @@
 "use strict"
 
 const PUYO_SIZE = 25;   //ぷよサイズ
+const WALL = 5;         //壁カラー
+let None = 0;         //なしマス
 
+
+const STAGE_HEIGHT = 19;
+const STAGE_WIDTH = 12;
 
 //マス目
 let board = 
 [
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,0,0,0,0,0,0,0,0,0,0,5],
-    [5,5,5,5,5,5,5,5,5,5,5,5],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,0,0,0,0,0,0,0,0,0,0,WALL],
+    [WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL,WALL],
 ];
 
-const WALL = 5;
+
+
+let puyo =
+[
+    [0,0],
+    [0,0]
+];
+
+
+
 
 // ################################################################
 // # ステージ
@@ -36,7 +53,19 @@ class Stage
 {
     constructor()
     {
+        None = color(0,0,0);
 
+        for(let y = 0; y < STAGE_HEIGHT; y++)
+        {
+            for(let x = 0; x < STAGE_WIDTH; x++)
+            {
+                if( board[y][x] != WALL)
+                {
+                    board[y][x] = None;
+                }
+
+            }
+        }
     }
 
 
@@ -67,109 +96,28 @@ class Stage
 
     Renderer()
     {
-//        console.log("あああ" + board.length);
-
-        for(let i = 0; i < board.length; i++)
+        for(let y = 0; y < board.length; y++)
         {
-            for(let j = 0; j <board[i].length; j++)
+            for(let x = 0; x <board[y].length; x++)
             {
-//                console.log("あああ\n");
-                if(board[i][j] == WALL)
+                if(board[y][x] == WALL)
                 {
                     fill(255,255,255);
-                    rect(j * PUYO_SIZE,i * PUYO_SIZE,PUYO_SIZE ,PUYO_SIZE);
+                    rect(x * PUYO_SIZE,y * PUYO_SIZE,PUYO_SIZE ,PUYO_SIZE);
                 }
+                else
+                {
+                    fill(board[y][x]);
+                    rect(x * PUYO_SIZE,y * PUYO_SIZE,PUYO_SIZE ,PUYO_SIZE);
+
+                }
+
+
+
             }
         }
-
-
-
-
     }
 
-}
-
-// ################################################################
-// # ぷよ
-// ################################################################
-class Puyo
-{
-    
-    getColor(rand)
-    {
-
-
-    }
-
-    constructor()
-    {        
-        this.colorType = Math.floor(random(0,5));   //種類 
-        this.position = new Vector(0,0);            //座標   
-    }
-
-    setPosition(pos)
-    {
-        this.position = pos;
-    }
-
-    getPosition()
-    {
-        return this.position;
-    }
-
-    Renderer() 
-    {
-        //色
-        switch(this.colorType)
-        {
-            //赤
-            case 0:
-                {
-                    fill(255,0,0);
-                }
-            break;
-
-            //青
-            case 1:
-                {
-                    fill(24,235,249);
-                }
-            break;
-
-
-            //黄色
-            case 2:
-                {
-                    fill(255,255,0);
-                }
-            break;
-
-            
-            //緑
-            case 3:
-                {
-                    fill(0,528,0);
-                }
-            break;
-
-            //紫
-            case 4:
-                {
-                    fill(0,0,528);
-                }
-            break;
-
-            default:
-                {
-                    console.log(this.colorType);
-                }
-
-        }
-
-//        noStroke();
-
-        circle(this.position.x * PUYO_SIZE,this.position.y * PUYO_SIZE,PUYO_SIZE);
-    }
 }
 
 // ################################################################
@@ -178,31 +126,338 @@ class Puyo
 class Control
 {
 
+    //　ぷよを設定
+    setPuyo()
+    {
+        this.colorType = new Array(2);
+        
+        //switch(Math.floor(random(0,5)))
+        switch(0)
+        {
+            //赤
+            case 0:
+                {
+                    this.colorType[0] = color(255,0,0);
+                }
+            break;
+
+            //青
+            case 1:
+                {
+                    this.colorType[0] = color(24,235,249);
+                }
+            break;
+
+            //黄色
+            case 2:
+                {
+                    this.colorType[0] = color(255,255,0);
+                }
+            break;
+            
+            //緑
+            case 3:
+                {
+                    this.colorType[0] = color(0,528,0);
+                }
+            break;
+
+            //紫
+            case 4:
+                {
+                    this.colorType[0] = color(192,48,192);
+                }
+            break;
+
+            default:
+                {
+                    console.log("this.colorType" + this.colorType);
+                }            
+        }
+
+
+        //switch(Math.floor(random(0,5)))
+        switch(0)
+        {
+            //赤
+            case 0:
+                {
+                    this.colorType[1] = color(255,0,0);
+                }
+            break;
+
+            //青
+            case 1:
+                {
+                    this.colorType[1] = color(24,235,249);
+                }
+            break;
+
+            //黄色
+            case 2:
+                {
+                    this.colorType[1] = color(255,255,0);
+                }
+            break;
+         
+            //緑
+            case 3:
+                {
+                    this.colorType[1] = color(0,528,0);
+                }
+            break;
+
+            //紫
+            case 4:
+                {
+                    this.colorType[1] = color(192,48,192);
+                }
+            break;
+
+            default:
+                {
+                    console.log("this.colorType" + this.colorType);
+                }            
+        }
+
+
+        puyo[0][0] = this.colorType[0];
+        puyo[1][0] = this.colorType[1];
+    }
+
+
+    //座標を初期化
+    Reset()
+    {
+        this.position = new Vector(6,1);
+    }
+
+
+    
     constructor()
     {
         this.position = new Vector(6,1);
         this.press = false;
         this.pressSpace = false;
-
         this.downSpeed = 200;
         this.time  = 0;
+        this.rotate = 0;
+        this.sideCollision = false;
+        this.setPuyo();
+        this.comboArray = new Array(0); //コンボ
+        this.comboList = new Array(0);
+        this.comboHistory = new Array(0);   //一時変数　コンボ履歴
+        this.comboFirst = true;
+        this.comboColor;
+        this.combo  = new Array(0);
+
     }
 
-    //座標を初期化
-    ResetPosition()
+    setStage()
     {
-        this.position = new Vector(6,1);
+        for(let yy = 0; yy < 2; yy++)
+        {
+            for(let xx = 0; xx < 2; xx++)
+            {
+                if(puyo[yy][xx] != 0)
+                {
+//                    console.log( "あああ "  + puyo[yy][xx]);
+                    board[this.position.y + yy - 2][this.position.x + xx - 1] = puyo[yy][xx];
+                }
+            } 
+        }
     }
 
+    // ################################ 連鎖対象のぷよ判定 ################################ 
+    isComboList(pos)
+    {
+        for(let i = 0; i < this.comboHistory.length; i++)
+        {
+            if(this.comboHistory[i].x == pos.x && this.comboHistory[i].y == pos.y)
+            {
+
+                return true;
+            }
+            
+//            console.log("isComboList:   "+ this.comboHistory[i].x + " , " +  this.comboHistory[i].y);
+            
+        }
+
+        return false;
+    }
+
+
+
+
+    // ################################ 連鎖判定 ################################ 
+    ComboCheck(pos,puyoColor)
+    {
+        if(this.comboFirst == true)
+        {
+            this.comboHistory.push(new Vector(pos.x,pos.y));
+            this.comboFirst = false;
+        }
+
+        if(board[pos.y + 1][pos.x].toString() == puyoColor.toString())    //Down
+        {
+            if ( this.isComboList(new Vector(pos.x,pos.y + 1)) == false )
+            {
+                this.p = new Vector(pos.x,pos.y + 1);
+                this.comboArray.push(new Vector(pos.x,pos.y + 1));
+                this.comboHistory.push(new Vector(pos.x,pos.y + 1));
+                console.log("Down   " + (this.p.x) + " , " + (this.p.y));
+
+                this.ComboCheck(this.p,puyoColor);
+            }
+        }
+
+        if(board[pos.y - 1][pos.x].toString() == puyoColor.toString())   //Up
+        {
+            if ( this.isComboList(new Vector(pos.x,pos.y - 1)) == false )
+            {
+                console.log("Up     " + (pos.x) + " , " + (pos.y - 1));
+
+                this.p = new Vector(pos.x,pos.y - 1);
+                this.comboArray.push(new Vector(pos.x,pos.y - 1));
+                this.comboHistory.push(new Vector(pos.x,pos.y - 1));
+                
+                this.ComboCheck(this.p,puyoColor);                
+            }
+        }
+  
+        if(board[pos.y][pos.x + 1].toString() == puyoColor.toString())   //Right
+        {
+            if ( this.isComboList(new Vector(pos.x + 1,pos.y)) == false )
+            {
+                console.log("Right      " + (pos.x + 1) + " , " + (pos.y));
+
+                this.p = new Vector(pos.x + 1,pos.y);
+                this.comboArray.push(new Vector(pos.x + 1,pos.y));
+                this.comboHistory.push(new Vector(pos.x + 1,pos.y));
+                
+                this.ComboCheck(this.p,puyoColor);                
+            }
+        }
+        
+        if(board[pos.y][pos.x - 1].toString() == puyoColor.toString())   //Left
+        {
+            if ( this.isComboList(new Vector(pos.x - 1,pos.y)) == false )
+            {
+                console.log("Left       " + (pos.x - 1) + " , " + (pos.y));
+
+                this.p = new Vector(pos.x - 1,pos.y);
+                this.comboArray.push(new Vector(pos.x - 1,pos.y));
+                this.comboHistory.push(new Vector(pos.x - 1,pos.y));
+                
+                this.ComboCheck(this.p,puyoColor);                
+            }
+        }        
+    }
+
+    Clear()
+    {
+        this.comboHistory.length = 0;
+
+        for(let y = 0; y < STAGE_HEIGHT; y++)
+        {
+            for(let x = 0; x < STAGE_WIDTH; x++)
+            {
+                if( (board[y][x] != WALL) && (board[y][x] != None) )
+                {
+
+                    this.comboArray.push(new Vector(x,y));
+
+                    this.ComboCheck(new Vector(x,y),board[y][x]);
+
+                    //連鎖配列に代入
+                    if(this.comboArray.length  > 3)
+                    {
+                        console.log("削除 !");
+                        this.comboArray.forEach((item) =>
+                        {
+                            this.combo.push(item);
+                        })
+                    }
+
+                    this.comboFirst = true;
+                    this.comboArray.length = 0;
+
+                    
+                }
+            }        
+        }
+        this.comboArray.length = 0;
+
+
+        for(let i = 0; i < this.combo.length; i++)
+        {
+            board[this.combo[i].y][this.combo[i].x] = None;
+        }
+
+//        console.log("");
+//        console.log("");
+//        console.log("");
+        
+    }
 
     Move()
     {
+         
+        //落下
+        this.time += deltaTime;
+        if(this.time > this.downSpeed)
+        { 
+            this.time = 0;
+            this.position.y += 1;
+
+            if(this.Collision() != None)
+            {
+                this.setStage();
+                this.Reset();
+
+                this.Clear();   //ぷよを消す
+
+
+
+
+            }
+        }
+
+        
         //スペースキー
         if(keyIsDown(" ".charCodeAt(0)) && (this.pressSpace == false) )
         {
+            this.rotate += 1;
+            if(this.rotate > 3)
+            {
+                  this.rotate = 0;
+            }
+
             this.pressSpace = true;
 
-            console.log("あああ");
+            for(let y = 0; y < 2; y++)
+            {
+                  for(let x = 0; x < 2; x++)
+                  {
+                        if(puyo[y][x] != 0)
+                        {
+//                            console.log("あああ\n");
+                              let xx = (cos(PI / 2 * this.rotate) * x) + (-sin(PI / 2 * this.rotate) * y);
+                              let yy = (sin(PI / 2 * this.rotate) * x) + (cos(PI / 2 * this.rotate) * y);
+
+                                
+                              console.log("     y " + Math.round(yy));
+                              console.log("     x " + Math.round(xx));
+
+  //                            console.log("y " + Math.round(yy + 1.5));
+  //                            console.log("x " + Math.round(xx + 1.5));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                              //puyo[Math.round(yy + 1.5)][Math.round((xx + 1.5))] = 1;
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                        }
+                  }
+            }
         }
         else if(keyIsDown(" ".charCodeAt(0)) == false)
         {
@@ -213,11 +468,21 @@ class Control
         if(keyIsDown(LEFT_ARROW) && (this.press == false) )
         {
             this.position.x += -1;
+            if(this.Collision() == WALL)
+            {
+                this.position.x += 1;
+            }
+           
             this.press = true;
         }
         else if(keyIsDown(RIGHT_ARROW) && (this.press == false) )
         {
             this.position.x += 1;
+            if(this.Collision() == WALL)
+            {
+                this.position.x += -1;
+            }
+            
             this.press = true;
         }
 
@@ -225,38 +490,39 @@ class Control
         {
             this.press = false;
         }
-        
-        //落下
-        this.time += deltaTime;
-        if(this.time > this.downSpeed)
-        {
-            this.time = 0;
-            this.position.y += 1;
-        }
+       
+
+
+
+
+
             
     }
     
+    //当たり判定
     Collision()
-    {
-        for(let i = 0; i < board.length; i++)
+    {        
+        for(let y = 0; y < 2; y++)
         {
-            for(let j = 0; j <board[i].length; j++)
+            for(let x = 0; x < 2; x++)
             {
-                if(board[i][j] != 0)
+                if(puyo[y][x] != 0)
                 {
-                    
+                    if(board[this.position.y + y][this.position.x + x - 1] != 0)
+                    {
+                        return board[this.position.y + y][this.position.x + x - 1];
+                    }
                 }
             }
-        }
-        
+        }        
+
+        return None;
     }
-
-
 
     Update()
     {
         this.Move();
-        this.Collision();
+        
     }
 
 
@@ -265,63 +531,31 @@ class Control
         return this.position;
     }
 
-}
-
-// ################################################################
-// # ぷよぷよ
-// ################################################################
-class PuyoPuyo
-{
-    constructor()
-    {
-        this.position = new Vector(0,0);
-        this.type = Math.floor(random(0,2));
-        this.puyo;
-
-        if(this.type == 0)
-        {
-            this.puyo = new Array(new Puyo,new Puyo); 
-        }
-        else
-        {
-            this.puyo = new Array(new Puyo,new Puyo,new Puyo,new Puyo); 
-        }
-    }
-
-    setPosition(pos)
-    {
-        this.position = pos;
-    }
-
-    Update()
-    {
-        if(this.type == 0)
-        {
-
-            this.puyo[0].setPosition(this.position);
-            this.puyo[1].setPosition(this.position.add(new Vector(0,1)));
-        }
-        else
-        {
-            this.puyo[0].setPosition(this.position);
-            this.puyo[1].setPosition(this.position.add(new Vector(0,1)));
-            this.puyo[2].setPosition(this.position.add(new Vector(1,1)));
-            this.puyo[3].setPosition(this.position.add(new Vector(1,2)));
-        }      
-
-
-        //console.log(this.puyo[0].getPosition().x);
-
-    }
 
     Renderer()
     {
-        this.puyo.forEach(function(item) 
+        for(let y = 0; y < 2; y++)
         {
-            item.Renderer();    
-        })
+            for(let x = 0; x < 2; x++)
+            {
+                if(puyo[y][x] != 0)
+                {
+                    fill(puyo[y][x]);
+                
+                    this.pos = new Vector(0,0);
+                    
+                    this.pos.x = (this.position.x * PUYO_SIZE) + (PUYO_SIZE * x);
+                    this.pos.y = (this.position.y * PUYO_SIZE) + (PUYO_SIZE * y);
+                
+                    circle(this.pos.x - (PUYO_SIZE / 2),this.pos.y - (PUYO_SIZE / 2),PUYO_SIZE);
+
+//                    console.log(this.position.x * PUYO_SIZE / PUYO_SIZE);
+                }
+            }
+        }
     }
 }
+
 
 // ################################################################
 // # ループ
@@ -332,7 +566,6 @@ class Game
 
     constructor()
     { 
-        this.puyo = new PuyoPuyo();
         this.control = new Control();
         this.stage = new Stage();
     }
@@ -340,15 +573,17 @@ class Game
     Update()
     {
         this.control.Update();
-        this.puyo.Update();
 
-        this.puyo.setPosition(this.control.getPosition());
-        this.stage.setPuyo(this.puyo);
     }
 
     Renderer()
     {
-        this.puyo.Renderer();
         this.stage.Renderer();
+        this.control.Renderer();
+
+
+//        fill(0,255,0);
+//        rect(PUYO_SIZE * 5,PUYO_SIZE * 16, PUYO_SIZE);
+    
     }    
 }
